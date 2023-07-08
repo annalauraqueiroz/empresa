667cdc90dd9da1ed03410a0b4b6bb268c615c584
Anna <annalauraqueiroz@hotmail.com>
2023-07-07 23:00:26 -0300

    summaries changed

--- webapi/Controllers/WeatherForecastController.cs
+++ webapi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Taylor Swift", "Fearless", "Speak Now", "Red", "1989", "Reputation", "Lover", "Folklore", "Evermore", "Midnights"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
